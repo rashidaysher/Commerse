e270cf25f90a894c5aaaca31ff826b9f09b0f8b8
unknown <rashidaisha.ara@gmail.com>
2026-05-06 15:43:17 +0300

    created the navbar, and the theme buttons.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Box } from "@chakra-ui/react";
 import {Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
+import Navbar from "./componenets/Navbar";
 
 function App() {
   return (
     <Box minH= '100vh'>
+        <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
          <Route path="/craete" element={<CreatePage />} />
--- Frontend/src/componenets/Navbar.jsx
+++ Frontend/src/componenets/Navbar.jsx
@@ -1,18 +1,44 @@
-import {Container } from "@chakra-ui/react";
+import { Container, Flex, Text, Button, HStack, useColorMode } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
 
-function Navbar() {
-  return <Container maxh={"1140px"} px={4}>
-    <Flex 
-    h={16}
-    alignItems={"center"}
-    justifyContent= {"space-between"}
-    flexdir= {{
-      base:"column",
-      sm: "row"
-    }}
-    >
-    </Flex>
-  </Container>;
-};
+const Navbar = function () {
+  const{ colorMode, toogleColorMode} = useColorMode();
+  return ( 
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize="2xl"
+          fontWeight="bold"
+          bgGradient="linear(to-r, purple.400, pink.400)"
+          bgClip="text"
+          textTransform={"uppercase"}
+          textAlign={"center"}
+        >
+          <Link to={"/"}>Product Store</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <CiSquarePlus fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick ={toogleColorMode}>{colorMode === "light"? "🌙" : "☀️"}
+              
+            </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+}
 
 export default Navbar;
